0b653aefb3f19aad64c30a2167e293aea102d340
Christina Placide <christina.placide@hotmail.com>
2020-03-24 20:58:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,8 +1,23 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
+require 'pry'
+# Implement me first!
   #
-  # Consult README for inputs and outputs
-end
+  # Consult README for inputs and outputs 
+  def find_item_by_name_in_collection(name, collection)
+  index = 0 
+  new_hash_with_add_ons = {}
+  #binding.pry 
+    while index < collection.size do 
+      if !new_hash_with_add_ons.has_key?(collection[index]) #true
+          new_hash_with_add_ons[collection[index] = collection[index]
+          [:name]
+        else
+          new_hash_with_add_ons[collection[index] += collection[index]
+          [:name]
+          index += 1
+    end
+    new_hash_with_add_ons
+ end
+ 
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
