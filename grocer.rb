66f76d0ec8c5c83278399c939f3b832d3f80a322
Christina Placide <christina.placide@hotmail.com>
2020-03-24 21:21:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,18 +4,12 @@ require 'pry'
   # Consult README for inputs and outputs 
   def find_item_by_name_in_collection(name, collection)
   index = 0 
-  new_hash_with_add_ons = {}
-  #binding.pry 
-    while index < collection.size do 
-      if !new_hash_with_add_ons.has_key?(collection[index]) #true
-          new_hash_with_add_ons[collection[index] = collection[index]
-          [:name]
-        else
-          new_hash_with_add_ons[collection[index] += collection[index]
-          [:name]
-          index += 1
-    end
-    new_hash_with_add_ons
+    while index < collection.length do 
+      if collection[index][:item] == name
+      return collection[index]
+    end  
+    index +=1
+      end
  end
  
 
