f15f0417629adf12fd22a7c0fbbc31acd28dbfcf
Christina Placide <christina.placide@hotmail.com>
2020-03-24 22:47:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ counter = 0
     else 
       cart_item_with_coupon = {
         :item => couponed_item_name,
-        :price => coupons[counter][:num] / coupons[counter][:num],
+        :price => coupons[counter][:cost] / coupons[counter][:num],
         :count => coupons[counter][:num],
         :clearance => cart_item[:clearance]
       }
