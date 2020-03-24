f9838d970712c33ce948efbb5a848d7340ed6a00
Christina Placide <christina.placide@hotmail.com>
2020-03-24 22:23:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,9 +35,8 @@ def consolidate_cart(cart)
    end  
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ 
+ 
 end
 
 def apply_clearance(cart)
