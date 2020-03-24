c2b497291b90eebc5be30b2987eadc316e1ec654
Christina Placide <christina.placide@hotmail.com>
2020-03-24 21:29:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,8 +17,22 @@ def consolidate_cart(cart)
  new_cart = []
  index = 0 
    while index < cart.length do 
-      new_cart_item = 
+      new_cart_item = find_item_by_name_in_collection(cart[index][:item], new_cart)
+      if new_cart_item != nil 
+      new_cart_item[:count] += 1 
+      else
+        new_cart_item = {
+          :item => cart[index][:item],
+          :price => cart[index][:price],
+          :clearance => cart[index][:clearance],
+          :count => 1 
+        }
+        new_cart << new_cart_item
    end
+    index += 1 
+   end
+    new_cart
+   end  
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
